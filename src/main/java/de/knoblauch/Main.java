c9c28494f2a39b769e6e53d40ac186d33eb82436
Steffen Knoblauch <knoblauch.steffen@web.de>
2025-12-04 17:10:50 +0100

    inital

---
+++ src/main/java/de/knoblauch/Main.java
@@ -0,0 +1,13 @@
+package de.knoblauch;
+
+
+import de.knoblauch.day1.Day1;
+
+public class Main {
+
+    static void main() {
+
+        Day1.execute();
+
+    }
+}
---
+++ src/main/java/de/knoblauch/day1/Day1.java
@@ -0,0 +1,9 @@
+package de.knoblauch.day1;
+
+public class Day1 {
+
+    public static void execute() {
+
+        IO.println("test day 1");
+    }
+}
